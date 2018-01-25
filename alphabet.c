cbf62f44ccf6aeec3534a7aa42fcb7ae543ae8f6
Shankar B <shankarb3061997@gmail.com>
2018-01-25 20:04:34 +0530

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int i;
 char a;
 printf("Enter a character:");
 scanf("%c",a);
-for(i=65;i<123;i++)
+if(a>='A' && a<='z')
+ {
+  printf("Alphabet");
+ }
+else
 {
-if(a==i)
-printf("Alphabet");
+  printf("No");
 }
-printf("Not an alphabet");
 return 0;
 }
