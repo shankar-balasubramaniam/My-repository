68ad53e07378cc78b4f54d2bf7f5166307c3a63d
Shankar B <shankarb3061997@gmail.com>
2018-01-26 12:23:03 +0530

    Create calc.c

---
+++ calc.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+char operator;
+printf("Enter two numbers:");
+scanf("%d%d",&a,&b);
+printf("Enter an operator(+,-,*,*):");
+scanf("%c",&operator);
+switch(operator)
+{
+case '+':
+ printf("Sum of two numbers is %d",a+b);
+case '-':
+ printf("Difference of ")
+}
+}
