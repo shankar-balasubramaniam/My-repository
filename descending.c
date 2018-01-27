3bdabb93f9e4602106c526d97d70c8bdce2c860f
Shankar B <shankarb3061997@gmail.com>
2018-01-27 12:18:28 +0530

    Create descending.c

---
+++ descending.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{int j,i,n,a[n],temp;
+printf("Enter the number of values to be entered:");
+scanf("%d",&n);
+printf("Enter the values:");
+for(i=0;i<n;i++)
+{scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{if(a[i]<a[j])
+
+{temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}}
+for(i=0;i<n;i++)
+{printf("%d\n",a[i]);
+}
+}
