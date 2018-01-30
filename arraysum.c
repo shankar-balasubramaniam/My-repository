1c3e3bde7429424c5c3e79698bc480146d5a5a3a
Shankar B <shankarb3061997@gmail.com>
2018-01-30 19:02:31 +0800

    Create arraysum.c

---
+++ arraysum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int N,K;
+int array[N];
+printf("Enter the size of array:");
+scanf("%d",&N);
+printf("For how many elements of the array do you want to find the sum?");
+scanf("%d",&K);
+printf("Enter the array:")
+for(i=0;i<N;i++)
+ {
+ scanf("%d",array[i]);
+ }
+for(i=0;i<K;i++)
+ {
+ sum=sum+
+ }
+}
