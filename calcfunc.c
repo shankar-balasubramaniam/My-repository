a0f8f905311f5c279503910839a415ed80672bc7
Shankar B <shankarb3061997@gmail.com>
2018-02-04 10:09:28 +0530

    Update calcfunc.c

--- calcfunc.c
+++ calcfunc.c
@@ -3,10 +3,6 @@ int main()
 {int a,b;
 printf("Enter two numbers:");
 scanf("%d%d",&a,&b);
-add(a,b);
-sub(a,b);
-mul(a,b);
-div(a,b);
 printf("The sum of %d and %d is %d\n",a,b,add(a,b));
 printf("The difference between %d and %d is %d\n",a,b,sub(a,b));
 printf("The product of %d and %d is %d\n",a,b,mul(a,b));
