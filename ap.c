a4a391d0a2f5fe0f26fa0159ad02f5b270920d8c
Shankar B <shankarb3061997@gmail.com>
2018-02-13 15:39:38 +0530

    Create ap.c

---
+++ ap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,a,d,i,val,sum=0;
+printf("Enter the no. of terms you want to print:");
+scanf("%d",&n);
+printf("Enter the first term:");
+scanf("%d",&a);
+printf("Enter the common difference:");
+scanf("%d",&d);
+for(i=0;i<n;i++)
+ {
+ val=
+ }
+}
