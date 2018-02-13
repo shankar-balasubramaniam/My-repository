408c4052ab509baf0aa51b66cb37f971572bf508
Shankar B <shankarb3061997@gmail.com>
2018-02-13 15:50:53 +0530

    Update ap.c

--- ap.c
+++ ap.c
@@ -2,7 +2,7 @@
 int main()
 {
 int n,a,d,i,val,sum=0;
-printf("Enter the no. of terms you want to print:");
+printf("Enter the no. of terms in the arithmetic progression:");
 scanf("%d",&n);
 printf("Enter the first term:");
 scanf("%d",&a);
@@ -10,6 +10,9 @@ printf("Enter the common difference:");
 scanf("%d",&d);
 for(i=0;i<n;i++)
  {
- val=
+ val=a+i*d;
+ sum+=val;
  }
+printf("The sum of %d terms of the arithmetic progression with first term %d and common difference %d is %d.",n,a,d,sum);
+return 0; 
 }
