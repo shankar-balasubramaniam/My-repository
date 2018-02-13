fc975421e5b0ec2063c478ecfbe086cfdf32aae9
Shankar B <shankarb3061997@gmail.com>
2018-02-13 16:15:50 +0530

    Rename greatest.c to greatestinarray.c

---
+++ greatestinarray.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int n,a[100],i,j,t;
+printf("Enter the number of elements you want in the array:");
+scanf("%d",&n);
+printf("Enter the array of %d elements:",n);
+for(i=0;i<n;i++)
+ {
+ scanf("%d",&a[i]);
+ }
+for(i=0;i<n;i++)
+ {
+ for(j=i+1;j<n;j++)
+  {
+  if(a[i]<a[j])
+   {
+   t=a[i];
+   a[i]=a[j];
+   a[j]=t;
+   }
+  }
+  break;
+ }
+printf("The greatest number in the array is %d",a[0]); 
+ return 0;
+}
