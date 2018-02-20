b4303b8b2ebebafb7dd3ac846ceb66cb7d1a2ffa
Shankar B <shankarb3061997@gmail.com>
2018-02-20 02:27:04 -0800

    Update median1.c

--- median1.c
+++ median1.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {int j,i,n,a[n],temp;
-printf("Enter the number of values to be entered:");
+printf("Enter the no. of values to be entered:");
 scanf("%d",&n);
 printf("Enter the values:");
 for(i=0;i<n;i++)
