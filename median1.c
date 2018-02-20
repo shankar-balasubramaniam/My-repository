6b724f42d2d4ed6ad6be083259ec7e8d2d22cb3d
Shankar B <shankarb3061997@gmail.com>
2018-02-20 02:24:36 -0800

    Create median1.c

---
+++ median1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{int j,i,n,a[n],temp;
+printf("Enter the number of values to be entered:");
+scanf("%d",&n);
+printf("Enter the values:");
+for(i=0;i<n;i++)
+{scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{if(a[i]>a[j])
+
+{temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}}
+printf("The median element in the array is %d",a[(n+1)/2-1]);
+}
