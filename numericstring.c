69cc3fc4506a020dbde07d96a433c9d752cf1a89
Shankar B <shankarb3061997@gmail.com>
2018-02-22 02:54:34 -0800

    Update numericstring.c

--- numericstring.c
+++ numericstring.c
@@ -1,27 +1,11 @@
 #include<stdio.h>
 int main()
 {
-char num[100];int n,numeric=1;
-printf("Enter the number of characters in your string...");
-scanf("%d",&n);
+char num;
 printf("Enter a string:");
-     for(i=0;i<n;i++)
-     {
-     scanf("%c",&num[i]);
-     }
-     for(i=0;i<n;i++)
-     {
-     if(num[i]>=48 && num[i]<=57)
-      {
-      numeric=1;
-      }
-     else
-      {
-      numeric=0;
-      break;
-      }
-     }
-if(numeric==1)
+scanf("%s",&num);    
+     
+if(num>=48 && num<=57)
  {
  printf("The string is numeric...");
  }
