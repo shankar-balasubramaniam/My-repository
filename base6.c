d61d60589456d993189b65f6d98272f896870ac0
Shankar B <shankarb3061997@gmail.com>
2018-07-26 10:25:23 +0530

    Update base6.c

--- base6.c
+++ base6.c
@@ -1,5 +1,27 @@
 //This is The Coding Area
 #include<stdio.h>
+int basesix(int dec)
+{
+  int mod,i,six=0;
+  for(i=1;dec!=0;i=i*10)
+  {
+  	mod=dec%6;
+  	dec=dec/6;
+  	six=mod*i+six;
+  }        
+  return six;
+}
+int digitsum(num)
+{
+  int digit,sum=0;
+   while (num > 0)
+    {
+        digit = num % 10;
+        sum  = sum + digit;
+        num /= 10;
+    }
+  return sum;
+}
 int main()
 {
   int n,a[100],i,j,count=0;
@@ -29,25 +51,4 @@ int main()
   
   return 0;
 }
-int basesix(int dec)
-{
-  int mod,i,six=0;
-  for(i=1;dec!=0;i=i*10)
-  {
-  	mod=dec%6;
-  	dec=dec/6;
-  	six=mod*i+six;
-  }        
-  return six;
-}
-int digitsum(num)
-{
-  int digit,sum=0;
-   while (num > 0)
-    {
-        digit = num % 10;
-        sum  = sum + digit;
-        num /= 10;
-    }
-  return sum;
-}
+
