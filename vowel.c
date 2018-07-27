12177a6bf035bf710b506fcd56b80721ebb76a67
Shankar B <shankarb3061997@gmail.com>
2018-07-27 13:48:01 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-char a;
-printf("Enter a letter:");
-scanf("%c",&a);
-if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
- {
- printf("Vowel");
- }
-else
- {
- printf("Consonant");
- }
-return 0; 
+    char a;
+    scanf("%c",&a);
+    if((a>='A' && a<='Z')||(a>='a' && a<='z'))
+    {
+        if(a=='a'||'A'||'e'||'E'||'i'||'I'||'o'||'O'||'u'||'U')
+            printf("vowel");
+        else
+            printf("consonant");
+    }   
+    else
+        printf("Enter a valid letter.");
+    return 0;
 }
