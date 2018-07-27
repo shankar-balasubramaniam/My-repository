159921afc310d86a8a2444d3a349fdcf0663f232
Shankar B <shankarb3061997@gmail.com>
2018-07-27 14:03:33 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -5,12 +5,12 @@ int main()
     scanf("%c",&a);
     if((a>='A' && a<='Z')||(a>='a' && a<='z'))
     {
-        if(a=='a'||'A'||'e'||'E'||'i'||'I'||'o'||'O'||'u'||'U')
+        if(a =='a'||a=='A'||a=='e'||a=='E'||a=='i'||a=='I'||a=='o'||a=='O'||a=='u'||a=='U')
             printf("vowel");
         else
             printf("consonant");
     }   
     else
-        printf("Enter a valid letter.");
+        printf("Enter a valid letter");
     return 0;
 }
